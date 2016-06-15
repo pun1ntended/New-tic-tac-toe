398e1a3c5e339964fc50a72a6af9fe5de935077d
RKoopman <raoul.koopman@gmail.com>
2016-06-15 13:55:19 -0400

    fixes winning methods

--- services/Winning.rb
+++ services/Winning.rb
@@ -1,43 +1,60 @@
 class Winning
-	WIN_COMBINATIONS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-]
 
-  def won?(board)
-    WIN_COMBINATIONS.each do |win_combo|
-    position_1 = board[win_combo[0]]
-    position_2 = board[win_combo[1]]
-    position_3 = board[win_combo[2]]
-    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
-      return win_combo
+  attr_accessor :positions, :cpu_icon, :play_icon, :board
+
+  WIN_COMBINATIONS = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ]
+    # => Move this to winning class
+  def win_condition
+    WIN_COMBINATIONS.each do |winning_combo|
+      if win_method?
+      else
+        turn_count(@board)
+        current_turn(@board) 
+      end
     end
   end
 
+  def win_method?
+    if winning_combo.all?{ |win_position| @player_moves.include?(win_position)} == true
+      @current_player.win
+      puts "humans > machines lol"
+      puts @current_player.stats
+      break
+    end
+  end
 
   def full?(board)
+    board = @board
     board.all? do |e|
       e == 'X' || e == 'O'
     end
   end
 ​
   def draw?(board)
-    !won?(board) && full?(board)
+    board = @board
+    !win_method?(board) && full?(board)
   end
 ​
   def over?(board)
-    won?(board) || draw?(board)
+    board = @board
+    win_method?(board) || draw?(board)
   end
 ​
   def winner(board)
+    board = @board
     winner = won?(board)
     if winner
       return board[winner[0]]
     end
-end
+  end
+
+end # <= closes class
